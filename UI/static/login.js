2860bc6d03a9853d963a6b53559d1a726befe0c3
root <root@localhost.localdomain>
2019-01-06 07:19:08 -0500

    configure fetch API with ireporter api [finishes #162952970]

---
+++ UI/static/login.js
@@ -0,0 +1,37 @@
+const url = "https://ireporter-version2.herokuapp.com/api/v2/auth/login"
+function login() {
+    event.preventDefault();
+    let Form = document.getElementById('login');
+    let loginFormData = new FormData('LoginForm');
+    
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({username: username, password:password})
+    })
+    .then((res)=>res.json())
+    //.tata) => console.log(data))
+    .then((data)=> {
+        //login sucess
+        if (data.status == 200){
+
+        localStorage.setItem('token', data.access_token)
+        localStorage.setItem('current_user', username)
+        window.location.replace("./user_profile.html")
+        }
+        else{
+            let error_container =  document.getElementById('errors')
+            if (error_container){
+                error_container.innerHTML = data.message
+            }
+        }
+        
+    })
+    .catch((e) => console.log(e))
+}
